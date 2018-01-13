e953961a7e1b18d81c0d60b1149e645753850a35
Marko Jevtic <marko.jevtic@prodyna.com>
2018-01-13 15:40:10 +0100

    init security-rest

---
+++ security-rest/src/test/java/pd/workshop/securityrest/SecurityRestApplicationTests.java
@@ -0,0 +1,16 @@
+package pd.workshop.securityrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
